8e7b94a2425b0b054e824d937b2fd228f72e4558
Seu Nome <leon.ueda2016@gmail.com>
2022-06-07 13:49:41 -0300

    accordion transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 import ScrollSuave from "./modules/scroll-suave.js";
-import initAnimacao from "./modules/scroll-animacao.js";
-import initAccorion from "./modules/accordion.js";
+import Accordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab-animacao.js";
 import initModal from "./modules/modal.js";
 import inittoolTip from "./modules/tooltip.js";
@@ -9,12 +8,15 @@ import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import initAnimacao from "./modules/scroll-animacao.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
+const accordion = new Accordion("[data-anime='accordion'] dt");
+accordion.init();
+
 initAnimacao();
-initAccorion();
 initTabNav();
 initModal();
 inittoolTip();
