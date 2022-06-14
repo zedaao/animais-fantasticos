3592500dbf8f09a420b9c2e4e6769e4a6fc5f15a
Seu Nome <leon.ueda2016@gmail.com>
2022-06-14 16:08:03 -0300

    fix

--- js/script.js
+++ js/script.js
@@ -37,5 +37,5 @@ initMenuMobile();
 initFuncionamento();
 initAnimacao();
 
-fetchAnimais("../../animaisapi.json", ".numeros-grid");
+fetchAnimais("././animaisapi.json", ".numeros-grid");
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
