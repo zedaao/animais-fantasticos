c95e247c76a1422201f5a78c17b536e642fb7f04
Seu Nome <leon.ueda2016@gmail.com>
2022-06-15 15:02:30 -0300

    fix

--- js/script.js
+++ js/script.js
@@ -42,7 +42,6 @@ const menuMobile = new MenuMobile(
 menuMobile.init();
 
 const funcionamento = new Funcionamento("[data-semana]", "aberto");
-console.log(funcionamento);
 funcionamento.init();
 
 initAnimacao();
