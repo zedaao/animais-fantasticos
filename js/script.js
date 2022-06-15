2706fa0afd1023d7f2ae781add9bdce8ef30112a
Seu Nome <leon.ueda2016@gmail.com>
2022-06-15 15:04:52 -0300

    fix

--- js/script.js
+++ js/script.js
@@ -45,6 +45,5 @@ const funcionamento = new Funcionamento("[data-semana]", "aberto");
 funcionamento.init();
 
 initAnimacao();
-
 fetchAnimais("../animaisapi.json", ".numeros-grid");
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
